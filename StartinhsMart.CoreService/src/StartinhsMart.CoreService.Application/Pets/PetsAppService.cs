252db32267d8bd46a88d5cd91e037940013b6d69
Lê Minh Tính <133968641+startinhs@users.noreply.github.com>
2025-08-06 15:18:37 +0700

    feat api get UrlImage

--- StartinhsMart.CoreService/src/StartinhsMart.CoreService.Application/Pets/PetsAppService.cs
+++ StartinhsMart.CoreService/src/StartinhsMart.CoreService.Application/Pets/PetsAppService.cs
@@ -18,6 +18,7 @@ using Volo.Abp.Caching;
 using Microsoft.Extensions.Caching.Distributed;
 using StartinhsMart.CoreService.Shared;
 using Volo.Abp.BlobStoring;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StartinhsMart.CoreService.Pets
 {
@@ -129,6 +130,25 @@ namespace StartinhsMart.CoreService.Pets
             var stream = await _blobContainer.GetAsync(fileDescriptor.Id.ToString("N"));
 
             return new RemoteStreamContent(stream, fileDescriptor.Name, fileDescriptor.MimeType);
+        }
+
+        [AllowAnonymous]
+        public virtual async Task<object> GetImageAsync(GetImageInput input)
+        {
+            var fileDescriptor = await _appFileDescriptorRepository.GetAsync(input.ImageId);
+            var stream = await _blobContainer.GetAsync(fileDescriptor.Id.ToString("N"));
+
+            if (stream == null)
+            {
+                return "Image not found.";
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+                var fileBytes = memoryStream.ToArray();
+                return new FileContentResult(fileBytes, fileDescriptor.MimeType);
+            }
         }
 
         public virtual async Task<AppFileDescriptorDto> UploadFileAsync(IRemoteStreamContent input)
